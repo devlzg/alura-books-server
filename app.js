5effcfae7e410cd10330d2738709e8dab5886270
Luiz Gabriel Lisboa Santos <lzg.lisboasantos5569@gmail.com>
2024-10-31 15:31:17 -0300

    adicionando permissao de cors

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const cors = require('cors');
+
 const app = express();
 const port = 8000;
 const rotaLivro = require("./rotas/livro.js");
+
+app.use(cors('http://localhost:3000'));
 app.use(express.json())
 
 app.use("/livros", rotaLivro);
